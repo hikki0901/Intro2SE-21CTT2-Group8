37ba7b9ecec585b5ba2568c97440302a4e025811
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2023-11-23 02:07:08 +0700

    Some test
    simple test on Contact page

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import './style.css';
 
 function Contact() {
     return(
-        <div>
-            This is the Contact page
-        </div>
+        <div className="contact-us-page">
+      <main>
+        <h2>Contact Us</h2>
+
+        <form action="/contact" method="post">
+          <input type="email" name="email" placeholder="Your email" />
+          <textarea name="message" placeholder="Drop us a feedback" />
+          <input type="submit" value="Send us" />
+        </form>
+      </main>
+    </div>
     );
 }
 
