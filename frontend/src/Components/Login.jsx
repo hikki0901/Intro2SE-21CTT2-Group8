acd423f6821cc857e6ede84beeae10c6c1f0a842
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2023-11-24 03:14:28 +0700

    Login page (90% progress)

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import "./Login.css"
 
 function Login() {
     return(
-        <div>
-            This is the Login page
+        <div class="container">
+            <main>
+                <div class="login-box">
+                    <div class="login-left">
+                        <h1>Dietarium</h1>
+                        <h2>Your one-stop solution for personalized health tracking and meal planning.</h2>
+                    </div>
+
+                    <div class="login-right">
+                        <div class="input-email">
+                            <input type="email" placeholder="Enter your Email"></input>
+                        </div>
+
+                        <div class="input-password">
+                            <input type="password" placeholder="Enter your Password"></input>
+                        </div>
+
+                        <button type="submit">Log in</button>
+
+                        <a href="#" class="forgot-pass">Forgot password?</a>
+
+                        <hr></hr>
+
+                        <button type="submit">Register</button>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
