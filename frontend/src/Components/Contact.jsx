1aff83b67ab569f8d87599594003e85c1404d018
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2023-11-23 22:17:56 +0700

    Contact page frontend complete

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
-import './style.css';
+import './Contact.css';
 
 function Contact() {
     return(
-        <div className="contact-us-page">
-      <main>
-        <h2>Contact Us</h2>
+      <div class="container">
+        <main>
+          <div class="contact-box">
+            <div class="contact-left">
+              <h1><u>Contact Us</u></h1>
+              <h2>Drop us a feedback</h2>
+              <p>We’re here to help! If you have any questions, concerns, or feedback, please reach out to us. We’ll get back to you as soon as possible.</p>
+            </div>
+            <div class="contact-right">
+              <form>
+                <div class="input-row">
+                  <div class="input-name">
+                    <input type="text" placeholder="Your name"></input>
+                  </div>
+                  <div class="input-email">
+                    <input type="email" placeholder="Your email"></input>
+                  </div>
+                </div>
 
-        <form action="/contact" method="post">
-          <input type="email" name="email" placeholder="Your email" />
-          <textarea name="message" placeholder="Drop us a feedback" />
-          <input type="submit" value="Send us" />
-        </form>
-      </main>
-    </div>
+                <div class="input-subject">
+                  <input type="text" placeholder="Subject"></input>
+                </div>
+
+                <textarea placeholder='Your message'></textarea>
+
+                <button type="submit">Send us</button>
+              </form>
+            </div>
+          </div>
+        </main>
+      </div>
     );
 }
 
