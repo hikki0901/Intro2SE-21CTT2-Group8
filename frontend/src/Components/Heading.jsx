04618f016def7950c396c8528917757cfab5b485
Huuhao1712 <nhhao21@apcs.fitus.edu.vn>
2023-11-25 22:15:11 +0700

    adjust header

--- frontend/src/Components/Heading.jsx
+++ frontend/src/Components/Heading.jsx
@@ -1,62 +1,34 @@
 import React from "react";
 import logo from "../image/Logo.png"
 import {Link} from 'react-router-dom'
+import "../CSS/Heading.css"
 
 function Heading(){
+    return (
+        <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <img src={logo} alt="logo image" class="bi me-2" width="40" height="32" role="img"/>
+          <span class="fs-4 app-name">DIETARIUM</span>
+        </a>
+      </div>
 
-    const headingStyle ={
-        height: "60px",
-        width: "1165px",
-        position: "relative",
-        left: "50%",
-        top: "40px",
-        transform: "translate(-50%, -50%)",
-        gap: "10px"
-    }
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><Link to ='/video' class="nav-link px-2">VIDEOS</Link></li>
+        <li><Link to ='/about' class="nav-link px-2">ABOUT</Link></li>
+        <li><Link to ='/contact' class="nav-link px-2">CONTACT</Link></li>
+      </ul>
 
-    const headingLine = {
-        position: "absolute",
-        right: "0%",
-        height: "60px",
-        display: "flex",
-        justifyContent: "flexEnd",
-        gap: "20px"
-    }
-
-    const appNameStyle = {
-        fontSize: "20px",
-        position: "absolute",
-        left: "25px",
-        fontFamily: "'Poppins', sans-serif",
-        fontWeight: "bold"
-    }
-
-    const headingTag = {
-        fontSize: "20px",
-        height: "30px",
-        top: "50%",
-        fontFamily: "'Poppins', sans-serif",
-    }
-
-    const logoStyle = {
-        position: "absolute",
-        top: "20px",
-        width: "22px",
-        height: "30.15px"
-    }
-
-    return <div className="heading" style={headingStyle}>
-        <img src = {logo}  style={logoStyle}/>
-        <p style={appNameStyle}>DIETARIUM</p>
-        <div style={headingLine}>
-            <Link to ='/home'><p style={headingTag}>HOME</p></Link>
-            <Link to ='/video'><p style={headingTag}>VIDEO</p></Link>
-            <Link to ='/about'><p style={headingTag}>ABOUT</p></Link>
-            <Link to ='/contact'><p style={headingTag}>CONTACT</p></Link>
-            <Link to ='/login'><p style={headingTag}>LOGIN</p></Link>
-            <Link to ='/signup'><p style={headingTag}>SIGNUP</p></Link>
-        </div>
-    </div>
+      <div class="col-md-2 text-end row">
+        <Link to ='/login' type="button" class="btn me-2 col-5">LOGIN</Link>
+        <Link type="button" class="btn col-5 btn-primary">SIGN UP</Link>
+      </div>
+    </header>
+  </div>
+    );
 }
+
+
 
 export default Heading
