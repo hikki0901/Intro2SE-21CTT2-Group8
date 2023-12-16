0d4365c2e66fa9a44e70968b3c45ec786b1305af
pnguyenhere <phnguyen.pn001@gmail.com>
2023-12-16 20:08:04 +0700

    register function (unfinished)

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -9,15 +9,23 @@ router.post("/register", async (req, res) => {
     const { firstName, lastName, DOB, phone, email, password, confirmPassword } = req.body;
     const user = await userModel.findOne({ email });
 
+    if (password !== confirmPassword) {
+        return res.status(400).json({ message: "Password and confirmPassword do not match" });
+    }
+
     if (user) {
         return res.json({ message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new userModel({firstName, lastName, DOB, phone, email, password:hashedPassword});
-    await newUser.save();
-
-    res.json({message: "User registered successfully"});
+    try {
+        await newUser.save();
+        res.json({ message: "User registered successfully" });
+    } catch (error) {
+        console.error("Error saving user:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
 
 router.post("/login", async (req, res) => {
@@ -31,7 +39,7 @@ router.post("/login", async (req, res) => {
         return res.status(401).json({ message: "User or password is invalid" });
     }
     const token = jwt.sign({ id: user._id}, "token");
-    return res.status(200).json({ success: true, message: "Login successful", token, userID: user._id });
+    res.status(200).json({ success: true, message: "Login successful", token, userID: user._id });
 });
 
 export { router as userRouter };
--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import signUpImg from "../image/SignUp.png"
 import "../CSS/Signup.css"
 
 function Signup() {
-    return(
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [DOB, setDOB] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+      try {
+        await axios.post("http://localhost:4000/auth/register", {
+          firstName,
+          lastName,
+          DOB,
+          email,
+          password,
+          confirmPassword,
+        });
+        alert ("Registration completed");
+      } catch (err) {
+        console.error("Error during login request:", err);
+      }
+  };
+
+  return(
       <div class="d-flex justify-content-center">
           <div class="col-md-4 text-center">
             <h1>Sign up</h1>
@@ -16,40 +42,75 @@ function Signup() {
             <div class="modal-body p-5 pt-0">
               <form class="">
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control rounded-3" id="floatingInput" placeholder=""/>
+                  <input 
+                  type="text" 
+                  class="form-control rounded-3" 
+                  id="floatingInput" 
+                  placeholder=""
+                  onChange={(event) => setfirstName(event.target.value)}
+                  />
                   <label for="floatingInput">First Name</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control rounded-3" id="lastName" placeholder=""/>
+                  <input 
+                  type="text" 
+                  class="form-control rounded-3" 
+                  id="lastName" 
+                  placeholder=""
+                  onChange={(event) => setlastName(event.target.value)}
+                  />
                   <label for="lastName">Last Name</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control rounded-3" id="birthDay" placeholder=""/>
+                  <input 
+                  type="date" 
+                  class="form-control rounded-3" 
+                  id="birthDay" 
+                  placeholder=""
+                  onChange={(event) => setDOB(event.target.value)}
+                  />
                   <label for="birthDay">Birth Day</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control rounded-3" id="email" placeholder="name@example.com"/>
+                  <input 
+                  type="email" 
+                  class="form-control rounded-3" 
+                  id="email" 
+                  placeholder="name@example.com"
+                  onChange={(event) => setEmail(event.target.value)}
+                  />
                   <label for="email">Email address</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="password" class="form-control rounded-3" id="password" placeholder="Password"/>
+                  <input type="password" 
+                  class="form-control rounded-3" 
+                  id="password" 
+                  placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
+                  />
                   <label for="password">Password</label>
                 </div>
 
                 <div class="form-floating mb-3">
-                  <input type="password" class="form-control rounded-3" id="confirm-password" placeholder="Password"/>
+                  <input 
+                  type="password" 
+                  class="form-control rounded-3" 
+                  id="confirm-password" 
+                  placeholder="Password"
+                  onChange={(event) => setConfirmPassword(event.target.value)}
+                  />
                   <label for="confirm-password">Confirm Password</label>
                 </div>
 
                 <hr/>
 
-                <button class="w-120 mb-2 button" type="submit">Sign up</button>
+                <button onClick= {onSubmit} class="w-120 mb-2 button" type="submit">Sign up</button>
               </form>
             </div>
           </div>
       </div>
         
-    );
+  );
 }
 
 export default Signup;
