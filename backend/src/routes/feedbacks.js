8ad8490e7a2d9c9251520a7d0f9ffadecbe32960
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2023-12-17 15:42:42 +0700

    Create feedbacks.js

---
+++ backend/src/routes/feedbacks.js
@@ -0,0 +1,20 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { feedbackModel } from "../models/feedbacksModel.js";
+
+const router = express.Router();
+
+router.post("/feedback", async (req, res) => {
+    const { name, email, subject, message } = req.body;
+    const newFeedback = new feedbackModel({name, email, subject, message});
+    try {
+        await newFeedback.save();
+        res.json({ message: "Feedback sent successfully" });
+    } catch (error) {
+        console.error("Error saving feedback:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+export { router as feedbackRouter };
