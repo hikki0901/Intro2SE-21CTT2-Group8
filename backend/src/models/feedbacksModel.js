b9a4b5f1cb34571d205abfb1733cb264ecde4427
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2023-12-17 15:23:57 +0700

    Create feedbacksModel.js

---
+++ backend/src/models/feedbacksModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const feedbackSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
+
+export const feedbackModel = mongoose.model("feedbacks", feedbackSchema);
