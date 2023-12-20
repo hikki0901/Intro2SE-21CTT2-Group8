5ad81897f299fc0fe5c8807f71c67e48de857402
dinh-quang-hieu <101045977+dinh-quang-hieu@users.noreply.github.com>
2023-12-20 22:41:14 +0700

    update dietDiary

--- frontend/src/Components/MealCard.jsx
+++ frontend/src/Components/MealCard.jsx
@@ -19,9 +19,27 @@ function MealCard_3(props){
     return (<div class="mealCard_3 p-2 mt-3">
         <p class="meal-name">{props.name}</p>
         <ul class="mt-4">{props.foodList}</ul>
-        <p class="intake">430 KCal</p>
+        <div class = "tg">
+            <p id ="tg1"  class ="target">target : 100%</p>
+            <p id="tg2" class="intake">430 KCal</p>
+        </div>
     </div>);
 }
 
+function MealCard_4(){
+    return (<div class="mealCard_4 p-2 mt-3">
+        <p  class="ttintake">Total intake</p>
+        <p class ="textStyle">690/1000 kcal</p>
+        <p style={{textAlign: 'center'}}>"Try harder"</p>
+    </div>);
+}
+
+function Suggestions() {
+    return (<div class="suggestCard p-2 mt-3">
+    <p  class="textSuggest">Suggests</p>
+    <p class ="suggest">"These small things: nutrition, place, climate, recreation, the whole casuistry of selfishness are inconceivably more important than everything one has taken to be important so far." Friedrich Nietzsche.</p>
+</div>);
+}
+
 export default MealCard
-export {MealCard_2, MealCard_3}
+export {MealCard_2, MealCard_3, MealCard_4,Suggestions}
--- frontend/src/Components/dietDiary.jsx
+++ frontend/src/Components/dietDiary.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState} from 'react';
 import SlideBar from "./SlideBar";
-import {MealCard_3} from "./MealCard";
+import {MealCard_3, MealCard_4, Suggestions} from "./MealCard";
 import Meals from '../data/Meals';
 import "../CSS/mealPlan.css"
 import {NavLink} from 'react-router-dom'
@@ -15,19 +15,86 @@ function createMealCard(mealItem){
     );
 }
 
+function createProgressCal() {
+    return(
+        <MealCard_4/>
+    )
+}
+
+function createSuggestions() {
+    return(
+        <Suggestions/>
+    )
+}
+
+
 function DietDiary(){
+
+    const [weight, setWeight] = useState("");
+    const [height, setHeight] = useState("");
+    const [bmi, setBmi] = useState("");
+
+    const handleCalcultion = (e) => {
+        e.preventDefault()
+
+        if(weight === 0 || height === 0) {
+            alert('Hello please enter a valid number')
+        }
+        else {
+            let bmiFormular = (weight/(height * height))
+            setBmi(bmiFormular.toFixed(2))
+        }     
+    }
 
     return(
     <div class="home-style row">
         <div class="col-2">
             <SlideBar class="col-3" />
         </div>
-        <div class="col-5">
-            <p class="plan">Today's plan</p>
-            {Meals.map(createMealCard)}
-        </div>
+        <div class = "col-10">
+            <div class ="row-2">
+                <div class="row">
+                    <p class="plan">Today's plan</p>
+                </div>
+                <div class =" row">
+                    <div class="col-5">
+                        {Meals.map(createMealCard)}
+                    </div>
 
-        
+                    <div class="col-5 ">
+                        <createProgressCal>
+                            <MealCard_4/>
+                        </createProgressCal>
+
+                        <createSuggestions>
+                            <Suggestions/>
+                        </createSuggestions>
+
+                        <div class ="bmiCal">
+                            <p class = "bmi">BMI Calculator</p>
+                            <div class ="row">
+                                <div class = "col-md-6">
+                                    <div >
+                                        <input class = "bmiCard" type='number' placeholder='Weight'value={weight} onChange={(e)=> setWeight(e.target.value)} />
+                                    </div>
+
+                                    <div >
+                                        <input class ="bmiCard" type='number' placeholder='Height'value={height} onChange={(e)=> setHeight(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className='col-md-6'>
+                                    <p class = "bmiResult" > Result : {bmi}</p>
+                                </div>
+                            </div>
+                            <div onSubmit={handleCalcultion} >
+                                <button  className='btn' type='submit'>Calculate</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     );
 }
