c22a1ad60e57db826942b87722037959ea6a3fb3
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2023-12-21 23:41:39 +0700

    membership page 1

--- frontend/src/Components/Membership.jsx
+++ frontend/src/Components/Membership.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import SlideBar from "./SlideBar";
+import "../CSS/membership.css"
 
 function Membership(){
     return(
         <div class="row">
         <div class="col-2">
             <SlideBar class="col-3" />
+        </div>
+        <div class="col-10">
+            <div class='membership-container'>
+                <h1>Join our premium course today!</h1>
+                <p>
+                🍏 Get a dedicated dietitian to craft a personalized plan tailored just for you. <br></br>
+                💬 Chat directly with your chosen dietitian whenever you need. <br></br>
+                🔄 Not vibing with your current dietitian? No worries! Switch to a new one hassle-free.
+                </p>
+                <div className='membership-content'>
+                    <div className='sub-content'>
+                        <h2>Starter</h2>
+                        <p>Free</p>
+                        <ul>
+                            <li>Access to our diet planner</li>
+                            <li>Customize your own plan</li>
+                            <li>Record and track your diet</li>
+                            <li>BMI and calories calculator</li>
+                            <li>Monthly diet analytics</li>
+                        </ul>
+                    </div>
+                    <div className='line'></div>
+                    <div className='sub-content'>
+                        <h2>Premium</h2>
+                        <p>$9.99/mo</p>
+                        <ul>
+                            <li>Features in starter course</li>
+                            <li>Access to our roster of professional dietitians</li>
+                            <li>Freely change dietitian to suit your style</li>
+                            <li>Contact with your dietitian via message or video call</li>
+                        </ul>
+                    </div>                
+                </div>
+                <button>Join now!</button>
+            </div>
         </div>
     </div>
     );
