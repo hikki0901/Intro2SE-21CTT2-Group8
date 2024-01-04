16d6862149d38f3c6600c701ae35c3c523cb43a7
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2024-01-04 22:55:11 +0700

    change notification key

--- backend/src/models/notificationsModel.js
+++ backend/src/models/notificationsModel.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const notificationSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
@@ -14,5 +18,7 @@ const notificationSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+notificationSchema.index({ email: 1, date: 1, subject: 1}, { unique: true });
 
 export const notificationModel = mongoose.model("notifications", notificationSchema);
