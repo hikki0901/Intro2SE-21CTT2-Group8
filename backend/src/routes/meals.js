fa0d6a2abee75337e4293d85bdbb96d9da4dc615
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2024-01-06 08:58:30 +0700

    add analytics

--- backend/src/routes/meals.js
+++ backend/src/routes/meals.js
@@ -64,7 +64,7 @@ const defaultMeal = {
   target: 0,
 };
 
-router.post("/dashboard", async (req, res) => {
+router.post("/analytics", async (req, res) => {
     const { email } = req.body;
     const Breakfast = await mealModel.findOne({ email, date: today, mealType: "Breakfast" });
     const Lunch = await mealModel.findOne({ email, date: today, mealType: "Lunch" });
@@ -74,9 +74,21 @@ router.post("/dashboard", async (req, res) => {
     const Lunch_y = await mealModel.findOne({ email, date: yesterday, mealType: "Lunch" });
     const Dinner_y = await mealModel.findOne({ email, date: yesterday, mealType: "Dinner" });
 
-    calories_today = Breakfast.calories + Lunch.calories + Dinner.calories
-    calories_yesterday = Breakfast_y.calories + Lunch_y.calories + Dinner_y.calories
-    calories_difference = calories_today - calories_yesterday
+    const calories = [Breakfast ? Breakfast.calories : 0, Lunch ? Lunch.calories : 0, Dinner ? Dinner.calories : 0,
+                      Breakfast_y ? Breakfast_y.calories : 0, Lunch_y ? Lunch_y.calories : 0, Dinner_y ? Dinner_y.calories : 0]
+
+    calories_difference = calories[0] + calories[1] + calories[2] - calories[3] - calories[4] - calories[5]
+
+    res.status(200).json({
+      calories_difference
+      });
+});
+
+router.post("/dashboard", async (req, res) => {
+    const { email } = req.body;
+    const Breakfast = await mealModel.findOne({ email, date: today, mealType: "Breakfast" });
+    const Lunch = await mealModel.findOne({ email, date: today, mealType: "Lunch" });
+    const Dinner = await mealModel.findOne({ email, date: today, mealType: "Dinner" });
 
     const meals = [
         {
