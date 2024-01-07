4b7a94c3d41e58fadcf2ee14e99a732b8fb603e5
dinh-quang-hieu <101045977+dinh-quang-hieu@users.noreply.github.com>
2024-01-08 01:30:14 +0700

    update small loading

--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -4,12 +4,15 @@ import "../CSS/loading.css"
 function Loading(props){
     return(
     <div class ="loading col-12">
-      <ClipLoader
-        color= "#36d7b7"
-        loading={props.loading}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"/>
+      <div >
+        <ClipLoader
+          color= "#36d7b7"
+          loading={props.loading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"/>
+        <p class="textl">Loading....</p>
+      </div>
     </div>
     )
 }
