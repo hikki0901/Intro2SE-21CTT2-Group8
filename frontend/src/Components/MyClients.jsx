95c11fe54936d38fc53295d0278c48faa82c61fb
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2024-01-08 16:16:26 +0700

    my clients

--- frontend/src/Components/MyClients.jsx
+++ frontend/src/Components/MyClients.jsx
@@ -1,8 +1,48 @@
 import React, { useState, useEffect, useRef } from 'react';
 import SlideBar from "./SlideBar";
-import "../CSS/myChat.css"
+import "../CSS/myClients.css"
+import UserCard from './UserCard';
+import search from '../image/search.png'
+import profile_pic from '../image/profile_pic.png'
+
+const usersData = [
+    { id: 1, name: 'User 1', progress:'75%' },
+    { id: 2, name: 'User 2', progress:'55%' },
+    { id: 3, name: 'User 3', progress:'45%' },
+    { id: 4, name: 'User 4', progress:'15%' },
+    // Add more users as needed
+  ];
 
 function MyClients(){
+
+    const [users, setUsers] = useState(usersData);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
+  
+    const handleUserClick = (user) => {
+      setSelectedUser(user);
+    };
+  
+    const handleSearchChange = (event) => {
+      setSearchTerm(event.target.value);
+    };
+  
+    const filteredUsers = users.filter(user =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    function createUserCard(userItem){
+        return(
+            <UserCard
+                key={userItem.id}
+                userType='user'
+                name={userItem.name}
+                image_link={profile_pic}
+                progress={userItem.progress}
+                onClick={() => handleUserClick(userItem)}
+            />
+        );
+    }
 
     return(
         <div class="row">
@@ -10,7 +50,62 @@ function MyClients(){
             <SlideBar userType="dietitian" class="col-3" />
         </div>
         <div class="col-10">
-            
+            <div className='my-clients'>
+                <div className="row">
+                    <div class="col-6">
+                        <div className='heading'>
+                            <h2>Your users</h2>
+                        </div>
+                        <div className='search'>
+                            <img src={search}></img>
+                            <input id='search-input' type="search" placeholder="Search" value={searchTerm} onChange={handleSearchChange} /> 
+                        </div>
+                        <div className='user-list-container'>
+                            {filteredUsers.map((createUserCard))}                            
+                        </div>      
+                    </div>
+                    <div class="col-6">
+                        {selectedUser && 
+                        <div className='client-info-container'>
+                            <div className='client-info'>
+                                <img src={profile_pic}></img>
+                                <div className='client-info-content'>
+                                    <p>Premium</p>
+                                    <h2>{selectedUser.name}</h2>
+                                </div>
+                            </div>
+                            <div className='client-details'>
+                                <div className='client-detail-section'>
+                                    <p>Date of birth:</p>
+                                    <p>01/01/2000</p>
+                                </div>
+                                <div className='client-detail-section'>
+                                    <p>Gender:</p>
+                                    <p>Female</p>
+                                </div>
+                                <div className='client-detail-section'>
+                                    <p>Height:</p>
+                                    <p>200cm</p>
+                                </div>
+                                <div className='client-detail-section'>
+                                    <p>Weight:</p>
+                                    <p>100kg</p>
+                                </div>
+                                <div className='client-detail-section'>
+                                    <p>BMI:</p>
+                                    <p>25</p>
+                                </div>
+                            </div>
+                            <div className='button-list'>
+                                <button>Plan</button>
+                                <button>Diary</button>
+                                <button>Analytics</button>
+                            </div>
+                        </div>
+                        }
+                    </div>
+                </div>                
+            </div>
         </div>
         <div class="verticalLine1" />
     </div>
--- frontend/src/Components/UserCard.jsx
+++ frontend/src/Components/UserCard.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import "../CSS/UserCard.css"
 
 function DietitianCard(props){
-    return (<div className=''>
+
+    const isUser = props.userType === "user";
+
+    return (<div className='' onClick={props.onClick}>
         <div className='dietitian-container'>
             <img src= {props.image_link} ></img>
+            {isUser ? 
+            <div>
+                <div className='dietitian-content'>
+                    <h2>{props.name}</h2>
+                    <div className="progress-info">
+                        <p class="progress-word">Progress</p>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style={{width: props.progress}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            : 
             <div className='dietitian-content'>
                 <h2>{props.name}</h2>
                 <p>{props.degree}</p>
-            </div>
+            </div> }
         </div>
     </div>)
 }
